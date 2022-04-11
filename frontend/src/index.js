85cc765dc4230068fa36e86cdbd5926a01ec67a9
paridv <pvaroshi@gmail.com>
2022-04-11 15:51:41 +0200

    Navbar bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
